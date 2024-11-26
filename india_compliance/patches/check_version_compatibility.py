c62f80c1de043556fda827731dc703f31504df70
Gurukrupa-Export <129185339+Gurukrupa-Export@users.noreply.github.com>
2024-11-26 18:22:02 +0530

    Update check_version_compatibility.py

--- india_compliance/patches/check_version_compatibility.py
+++ india_compliance/patches/check_version_compatibility.py
@@ -23,37 +23,37 @@ VERSIONS_TO_COMPARE = [
 ]
 
 
-def execute():
-    for app in VERSIONS_TO_COMPARE:
-        app_name = app["app_name"]
-        app_version = app["current_version"]
+# def execute():
+#     for app in VERSIONS_TO_COMPARE:
+#         app_name = app["app_name"]
+#         app_version = app["current_version"]
 
-        if IC_VERSION.major != app_version.major:
-            show_error_and_exit(
-                f"Incompatible {app_name} Version: \n"
-                f"{app_name} version {app_version} not compatible with India Compliance {IC_VERSION}\n"
-                f"Please switch to version {IC_VERSION.major} of {app_name} to use the current version of"
-                " India Compliance.\n"
-            )
+#         if IC_VERSION.major != app_version.major:
+#             show_error_and_exit(
+#                 f"Incompatible {app_name} Version: \n"
+#                 f"{app_name} version {app_version} not compatible with India Compliance {IC_VERSION}\n"
+#                 f"Please switch to version {IC_VERSION.major} of {app_name} to use the current version of"
+#                 " India Compliance.\n"
+#             )
 
-        if IC_VERSION.major == 16:
-            continue
+#         if IC_VERSION.major == 16:
+#             continue
 
-        app_branch = get_app_branch(app_name.lower())
-        required_versions = app["required_versions"]
+#         app_branch = get_app_branch(app_name.lower())
+#         required_versions = app["required_versions"]
 
-        if app_branch not in required_versions:
-            continue
+#         if app_branch not in required_versions:
+#             continue
 
-        required_version = version.parse(required_versions[app_branch])
+#         required_version = version.parse(required_versions[app_branch])
 
-        if app_version < required_version:
-            show_error_and_exit(
-                f"Incompatible {app_name} Version: \n"
-                f"{app_name} version {app_version} not compatible with India Compliance {IC_VERSION}\n"
-                "Please upgrade {app_name} to version {required_version} or above to use the current version of"
-                " India Compliance.\n"
-            )
+#         if app_version < required_version:
+#             show_error_and_exit(
+#                 f"Incompatible {app_name} Version: \n"
+#                 f"{app_name} version {app_version} not compatible with India Compliance {IC_VERSION}\n"
+#                 "Please upgrade {app_name} to version {required_version} or above to use the current version of"
+#                 " India Compliance.\n"
+#             )
 
 
 def show_error_and_exit(error_message):
